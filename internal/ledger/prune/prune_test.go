0ec5487c130579d4d8e18fa6adaad7e4d448822b
huangwei <koiweiwork@google.com>
2024-03-08 16:16:19 +0800

    feat: opt prune flush strategy

--- internal/ledger/prune/prune_test.go
+++ internal/ledger/prune/prune_test.go
@@ -1,8 +1,10 @@
 package prune
 
 import (
+	"crypto/rand"
 	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -225,9 +227,154 @@ func TestTrieCacheRollback(t *testing.T) {
 
 }
 
+func TestPruningFlushByMaxFlushNum(t *testing.T) {
+	logger := log.NewWithModule("prune_test")
+	repoRoot := t.TempDir()
+	pStateStorage, err := pebble.New(filepath.Join(repoRoot, "pLedger"), nil, nil)
+	assert.Nil(t, err)
+
+	tc := NewTrieCache(createMockRepo(t), pStateStorage, logger)
+	for i := 0; i < tc.rep.Config.Ledger.StateLedgerReservedHistoryBlockNum+maxFlushBlockNum-1; i++ {
+		trieJournal1 := types.TrieJournalBatch{
+			&types.TrieJournal{
+				Version: uint64(i + 1),
+				DirtySet: map[string][]byte{
+					"k1": []byte("v1"),
+				},
+				PruneSet: map[string]struct{}{},
+			},
+		}
+		tc.Update(uint64(i+1), trieJournal1)
+	}
+	time.Sleep(time.Second)
+	v, ok := tc.Get(uint64(1), []byte("k1"))
+	require.True(t, ok)
+	require.Equal(t, []byte("v1"), v)
+	v, ok = tc.Get(uint64(tc.rep.Config.Ledger.StateLedgerReservedHistoryBlockNum+maxFlushBlockNum-1), []byte("k1"))
+	require.True(t, ok)
+	require.Equal(t, []byte("v1"), v)
+	require.Nil(t, tc.ledgerStorage.Get([]byte("k1"))) // not flush
+
+	trieJournal2 := types.TrieJournalBatch{
+		&types.TrieJournal{
+			Version: 2,
+			DirtySet: map[string][]byte{
+				"k1": []byte("v11"),
+			},
+		},
+	}
+	tc.Update(uint64(tc.rep.Config.Ledger.StateLedgerReservedHistoryBlockNum), trieJournal2)
+	time.Sleep(checkFlushTimeInterval + time.Second)
+	v, ok = tc.Get(uint64(tc.rep.Config.Ledger.StateLedgerReservedHistoryBlockNum), []byte("k1"))
+	require.True(t, ok)
+	require.Equal(t, []byte("v11"), v)
+	require.Equal(t, []byte("v1"), tc.ledgerStorage.Get([]byte("k1"))) // backend flushed
+	require.Equal(t, tc.rep.Config.Ledger.StateLedgerReservedHistoryBlockNum, len(tc.states.diffs))
+
+}
+
+func TestPruningFlushByMaxBatchSize(t *testing.T) {
+	logger := log.NewWithModule("prune_test")
+	repoRoot := t.TempDir()
+	pStateStorage, err := pebble.New(filepath.Join(repoRoot, "pLedger"), nil, nil)
+	assert.Nil(t, err)
+
+	tc := NewTrieCache(createMockRepo(t), pStateStorage, logger)
+	bigV := make([]byte, maxFlushBatchSizeThreshold)
+	rand.Read(bigV)
+	trieJournal := types.TrieJournalBatch{
+		&types.TrieJournal{
+			Version: uint64(1),
+			DirtySet: map[string][]byte{
+				"k1": bigV,
+			},
+		},
+	}
+	tc.Update(uint64(1), trieJournal)
+
+	for i := 1; i < tc.rep.Config.Ledger.StateLedgerReservedHistoryBlockNum; i++ {
+		trieJournal := types.TrieJournalBatch{
+			&types.TrieJournal{
+				Version: uint64(i + 1),
+				DirtySet: map[string][]byte{
+					"k1": []byte("v1"),
+				},
+			},
+		}
+		tc.Update(uint64(i+1), trieJournal)
+	}
+	time.Sleep(time.Second)
+	v, ok := tc.Get(uint64(1), []byte("k1"))
+	require.True(t, ok)
+	v, ok = tc.Get(uint64(tc.rep.Config.Ledger.StateLedgerReservedHistoryBlockNum), []byte("k1"))
+	require.True(t, ok)
+	require.Equal(t, []byte("v1"), v)
+	require.Nil(t, tc.ledgerStorage.Get([]byte("k1"))) // not flush
+
+	trieJournal2 := types.TrieJournalBatch{
+		&types.TrieJournal{
+			Version: 2,
+			DirtySet: map[string][]byte{
+				"k1": []byte("v11"),
+			},
+		},
+	}
+	tc.Update(uint64(tc.rep.Config.Ledger.StateLedgerReservedHistoryBlockNum+1), trieJournal2)
+	time.Sleep(2*checkFlushTimeInterval + time.Second)
+	v, ok = tc.Get(uint64(tc.rep.Config.Ledger.StateLedgerReservedHistoryBlockNum+1), []byte("k1"))
+	require.True(t, ok)
+	require.Equal(t, []byte("v11"), v)
+	v = tc.ledgerStorage.Get([]byte("k1"))
+	require.True(t, len(v) > 0)
+	require.Equal(t, bigV, v) // flushed
+	require.Equal(t, tc.rep.Config.Ledger.StateLedgerReservedHistoryBlockNum, len(tc.states.diffs))
+
+}
+
+func TestPruningFlushByMaxFlushTime(t *testing.T) {
+
+	t.Skip()
+
+	logger := log.NewWithModule("prune_test")
+	repoRoot := t.TempDir()
+	pStateStorage, err := pebble.New(filepath.Join(repoRoot, "pLedger"), nil, nil)
+	assert.Nil(t, err)
+
+	tc := NewTrieCache(createMockRepo(t), pStateStorage, logger)
+
+	for i := 0; i < tc.rep.Config.Ledger.StateLedgerReservedHistoryBlockNum+1; i++ {
+		trieJournal := types.TrieJournalBatch{
+			&types.TrieJournal{
+				Version: uint64(i + 1),
+				DirtySet: map[string][]byte{
+					"k1": []byte("v1"),
+				},
+			},
+		}
+		tc.Update(uint64(i+1), trieJournal)
+	}
+	time.Sleep(time.Second)
+	v, ok := tc.Get(uint64(1), []byte("k1"))
+	require.True(t, ok)
+	v, ok = tc.Get(uint64(tc.rep.Config.Ledger.StateLedgerReservedHistoryBlockNum+1), []byte("k1"))
+	require.True(t, ok)
+	require.Equal(t, []byte("v1"), v)
+	require.Nil(t, tc.ledgerStorage.Get([]byte("k1"))) // not flush
+
+	time.Sleep(maxFlushTimeInterval + time.Second)
+	v, ok = tc.Get(uint64(tc.rep.Config.Ledger.StateLedgerReservedHistoryBlockNum+1), []byte("k1"))
+	require.True(t, ok)
+	require.Equal(t, []byte("v1"), v)
+	v = tc.ledgerStorage.Get([]byte("k1"))
+	require.True(t, len(v) > 0)
+	require.Equal(t, []byte("v1"), v) // flushed
+	require.Equal(t, tc.rep.Config.Ledger.StateLedgerReservedHistoryBlockNum, len(tc.states.diffs))
+
+}
+
 func createMockRepo(t *testing.T) *repo.Repo {
 	r, err := repo.Default(t.TempDir())
 	require.Nil(t, err)
-	r.Config.Ledger.StateLedgerHistoryBlockNumber = 10
+	r.Config.Ledger.StateLedgerReservedHistoryBlockNum = 10
 	return r
 }
--- internal/ledger/prune/prunner.go
+++ internal/ledger/prune/prunner.go
@@ -1,13 +1,13 @@
 package prune
 
 import (
-	"github.com/axiomesh/axiom-ledger/pkg/repo"
 	"time"
 
 	"github.com/sirupsen/logrus"
 
 	"github.com/axiomesh/axiom-kit/storage"
 	"github.com/axiomesh/axiom-ledger/internal/ledger/utils"
+	"github.com/axiomesh/axiom-ledger/pkg/repo"
 )
 
 type prunner struct {
@@ -22,9 +22,10 @@ type prunner struct {
 
 const (
 	defaultMinimumReservedBlockNum = 2
-	defaultFlushBlockNum           = 16
-	defaultFlushTimeInterval       = 1 * time.Second
-	defaultFlushBatchSize          = 128 * 1024 * 1024 // 128MB
+	maxFlushBlockNum               = 240
+	checkFlushTimeInterval         = 5 * time.Second
+	maxFlushTimeInterval           = 2 * time.Minute
+	maxFlushBatchSizeThreshold     = 12 * 1024 * 1024 // 12MB
 )
 
 func NewPrunner(rep *repo.Repo, ledgerStorage storage.Storage, states *states, logger logrus.FieldLogger) *prunner {
@@ -33,6 +34,7 @@ func NewPrunner(rep *repo.Repo, ledgerStorage storage.Storage, states *states, l
 		ledgerStorage: ledgerStorage,
 		states:        states,
 		logger:        logger,
+		lastPruneTime: time.Now(),
 	}
 }
 
@@ -43,55 +45,87 @@ func (p *prunner) pruning() {
 	if p.rep.Config.Ledger.StateLedgerReservedHistoryBlockNum > reserve {
 		reserve = p.rep.Config.Ledger.StateLedgerReservedHistoryBlockNum
 	}
-	ticker := time.NewTicker(defaultFlushTimeInterval)
+
+	var (
+		ticker               = time.NewTicker(checkFlushTimeInterval)
+		pendingBatch         = p.ledgerStorage.NewBatch()
+		from                 uint64 // block from
+		to                   uint64 // block to
+		pendingFlushBlockNum uint32
+	)
+
+	flushFunc := func() {
+		if pendingFlushBlockNum <= 0 || pendingBatch.Size() <= 0 {
+			return
+		}
+		pendingBatch.Put(utils.CompositeKey(utils.TrieJournalKey, utils.MinHeightStr), utils.MarshalHeight(to+1))
+		pendingBatch.Commit()
+
+		//reset states diff
+		{
+			p.states.lock.Lock()
+			defer p.states.lock.Unlock()
+			p.states.diffs = p.states.diffs[pendingFlushBlockNum:]
+			p.states.size.Add(int32(-pendingFlushBlockNum))
+			p.logger.Infof("[Prune] prune state from block %v to block %v", from, to)
+		}
+
+		// reset tmp var
+		pendingBatch = p.ledgerStorage.NewBatch()
+		from = 0
+		to = 0
+		pendingFlushBlockNum = 0
+		p.lastPruneTime = time.Now()
+	}
 
 	for {
 		select {
 		case <-ticker.C:
-			if int(p.states.size.Load()) < defaultFlushBlockNum+reserve {
+			if int(p.states.size.Load()) <= reserve {
 				break
 			}
-			stales := p.states.diffs[:defaultFlushBlockNum]
-			from, to := stales[0].height, stales[len(stales)-1].height
-			batch := p.ledgerStorage.NewBatch()
-			pruneSet := make(map[string]struct{})
-			writeSet := make(map[string]struct{})
-			// merge prune set and write set, reduce duplicated entries
-			for _, diff := range stales {
-				for _, journal := range diff.trieJournals {
-					for k := range journal.PruneSet {
-						pruneSet[k] = struct{}{}
-					}
-					for k := range journal.DirtySet {
-						writeSet[k] = struct{}{}
-					}
+			pendingStales := p.states.diffs[pendingFlushBlockNum : len(p.states.diffs)-reserve]
+			if len(pendingStales) > 0 {
+				if from == 0 {
+					from = pendingStales[0].height
 				}
-			}
-			for _, diff := range stales {
-				for _, journal := range diff.trieJournals {
-					for k := range journal.PruneSet {
-						if _, ok := writeSet[k]; !ok {
-							batch.Delete([]byte(k))
+				to = pendingStales[len(pendingStales)-1].height
+
+				pruneSet := make(map[string]struct{})
+				writeSet := make(map[string]struct{})
+				// merge prune set and write set, reduce duplicated entries
+				for _, diff := range pendingStales {
+					for _, journal := range diff.trieJournals {
+						for k := range journal.PruneSet {
+							pruneSet[k] = struct{}{}
+						}
+						for k := range journal.DirtySet {
+							writeSet[k] = struct{}{}
 						}
 					}
-					for k, v := range journal.DirtySet {
-						if _, ok := pruneSet[k]; !ok {
-							batch.Put([]byte(k), v)
+
+				}
+				for _, diff := range pendingStales {
+					for _, journal := range diff.trieJournals {
+						for k := range journal.PruneSet {
+							if _, ok := writeSet[k]; !ok {
+								pendingBatch.Delete([]byte(k))
+							}
+						}
+						for k, v := range journal.DirtySet {
+							if _, ok := pruneSet[k]; !ok {
+								pendingBatch.Put([]byte(k), v)
+							}
 						}
 					}
+					pendingBatch.Delete(utils.CompositeKey(utils.TrieJournalKey, diff.height))
+					pendingFlushBlockNum++
 				}
-				batch.Delete(utils.CompositeKey(utils.TrieJournalKey, diff.height))
 			}
-			batch.Put(utils.CompositeKey(utils.TrieJournalKey, utils.MinHeightStr), utils.MarshalHeight(to+1))
-			batch.Commit()
 
-			p.states.lock.Lock()
-			p.states.diffs = p.states.diffs[defaultFlushBlockNum:]
-			p.states.size.Add(-defaultFlushBlockNum)
-			p.states.lock.Unlock()
-
-			p.lastPruneTime = time.Now()
-			p.logger.Infof("[Prune] prune state from block %v to block %v", from, to)
+			if time.Since(p.lastPruneTime) >= maxFlushTimeInterval || pendingFlushBlockNum >= maxFlushBlockNum || pendingBatch.Size() > maxFlushBatchSizeThreshold {
+				flushFunc()
+			}
 		}
 	}
 }
